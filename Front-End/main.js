2b896a685e77dd15229278b412b9428aeb643e2c
crohnaro <crohnarodev@gmail.com>
2022-10-07 08:09:35 -0300

    Finalizando projeto, adicionando metodo put

--- Front-End/main.js
+++ Front-End/main.js
@@ -1,7 +1,9 @@
 const API_URL = 'http://localhost:8080/api/products'
 
-const productsList = document.querySelector ('#products-list');
-const form = document.querySelector ('#form');
+const edit = document.querySelector('#edit')
+const productsList = document.querySelector ('#products-list')
+const form = document.querySelector ('#form')
+const formEdit = document.querySelector('#formEdit')
 
 function adicionaEventoBotaoExcluir(){
     const botoesExcluir = document.querySelectorAll('.botao-excluir')
@@ -32,32 +34,17 @@ function adicionaEventoBotaoEditar(){
     botoesEditar.forEach (botao => {
         botao.onclick = function(e){
             e.preventDefault()
+            edit.classList.remove('hidden')
 
-            const name = document.forms['formEdit'].name.value
-            const brand = document.forms['formEdit'].brand.value
-            const price = document.forms['formEdit'].price.value
             const id = this.dataset.id
-                    
-            fetch(`${API_URL}/${id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    name,
-                    brand,
-                    price,
-                })
-            }).then(response => {
-                response.json().then(data => {
-                    if (data.message === 'success') {
-                        obterLista()
-                        alert ('Produto editado!')
-                    } else {
-                        alert ('Ocorreu um erro')
-                    }
-                })
-            })
+            const name = this.dataset.name
+            const brand = this.dataset.brand
+            const price = this.dataset.price
+
+            document.forms['formEdit'].id.value = id
+            document.forms['formEdit'].name.value = name 
+            document.forms['formEdit'].brand.value = brand 
+            document.forms['formEdit'].price.value = price        
         }
     })
 }
@@ -68,8 +55,22 @@ function obterLista() {
             const productsHTML = data.map(product => `
             <li>
                 ${product.name} - ${product.brand} - ${product.price}
-                <a href="#" class="botao-excluir" data-id="${product._id}">[excluir]</a>
-                <a href="#" class="botao-editar" data-id="${product._id}">[editar]</a> 
+                <a 
+                    href="#" 
+                    class="botao-excluir" 
+                    data-id="${product._id}" 
+                    >
+                    [excluir]
+                </a>
+                <a 
+                    href="#" 
+                    class="botao-editar" 
+                    data-id="${product._id}"
+                    data-name="${product.name}" 
+                    data-brand="${product.brand}" 
+                    data-price="${product.price}">
+                    [editar]
+                </a> 
             </li>    
             `).join('')
             
@@ -109,6 +110,38 @@ form.onsubmit = function(e) {
                 alert('Cadastro realizado com sucesso')
             } else {
                 alert('Ocorreu um erro')
+            }
+        })
+    })
+}
+
+formEdit.onsubmit = function (e){
+    e.preventDefault
+    
+    const name = document.forms['formEdit'].name.value
+    const brand = document.forms['formEdit'].brand.value
+    const price = document.forms['formEdit'].price.value
+    const id = document.forms['formEdit'].id.value
+            
+    fetch(`${API_URL}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            brand,
+            price,
+        })
+    }).then(response => {
+        response.json().then(data => {
+            if (data.message === 'success') {
+                formEdit.reset()
+                edit.classList.add('hidden')
+                obterLista()
+                alert ('Produto alterado')
+            } else {
+                alert ('Ocorreu um erro')
             }
         })
     })
